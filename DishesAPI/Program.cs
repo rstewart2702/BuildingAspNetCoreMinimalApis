0251b2208b3d709af932ea46e2d8d03c15d8f2ac
Richard S. Stewart <rstewart2702@gmail.com>
2025-02-10 15:20:57 -0600

    WIP:  more comments, plus code necessary for EF migrations.

--- DishesAPI/Program.cs
+++ DishesAPI/Program.cs
@@ -1,6 +1,27 @@
+using DishesAPI.DbContexts;
+using DishesAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+// THIS SOURCE FILE MUST RESULT IN A TOP-LEVEL, "MAIN" CLASS
+// WHICH CONFORMS TO SOME PRE-DETERMINED EXPECTATION,
+// WHICH MAKES IT POSSIBLE FOR THINGS LIKE THE EF-CORE
+// MIGRATIONS TO FIND WHAT IT NEEDS TO GENERATE 
+// DATABASE STRUCTURE-MIGRATIONS PROGRAMS BY EXAMINING 
+// COMPILED ASSEMBLIES?
+
+// Hmmm:  so, this is actually going to result in something like
+//        a pre-defined args (and precisely where it is defined is a present mystery...)
+//        and yet this results in something which the enclosing system can interrogate
+//        and process.
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// register the DbContext on the container, getting the
+// connection string from appSettings
+builder.Services.AddDbContext<DishesDbContext>(
+    o => o.UseSqlite(builder.Configuration["ConnectionStrings:DishesDBConnectionString"])
+);
 
 var app = builder.Build();
 
@@ -25,6 +46,16 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 });
+
+
+// Added by the instructor at almost the end of the "Demo:  Adding the Data Layer" 
+// lesson:
+// "recreate & migrate the database on each run, for demo purposes"
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope()) {
+    var context = serviceScope.ServiceProvider.GetRequiredService<DishesDbContext>();
+    context.Database.EnsureDeleted();
+    context.Database.Migrate();
+}
 
 app.Run();
 
