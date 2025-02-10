0e8f7a0d0217da14f563eab0586e3f07da23df7c
Richard S. Stewart <rstewart2702@gmail.com>
2025-02-10 17:09:20 -0600

    Initial change:  defining a starting endpoint/route.

--- DishesAPI/Program.cs
+++ DishesAPI/Program.cs
@@ -34,17 +34,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/dishes", (DishesDbContext context) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return context.Dishes;
 });
 
 
@@ -59,7 +51,3 @@ using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().Create
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
